d67c6706569cb9809e5ed2bf665dd9a209af5980
Pedro Maltez <maltezpedro@gmail.com>
2013-03-26 18:13:26 +0000

    Advanced calculator and divide by 0 error implemented

--- main.rb
+++ main.rb
@@ -4,6 +4,35 @@ def re_do
 	gets.chomp.downcase == "y"
 end
 
+#Declaring methods
+def addition (x,y)
+	puts "Your operation is: #{x} + #{y}"
+	x + y
+end
+
+def subtraction (x,y)
+	puts "Your operation is: #{x} - #{y}"
+	x - y
+end
+
+def multiplication (x,y)
+	puts "Your operation is: #{x} * #{y}"
+	x * y
+end
+
+def division (x,y)
+	puts "Your operation is: #{x} / #{y}"
+	puts x / y
+end
+
+def square_root (x)
+	puts "The square root of #{x} is #{Math.sqrt(x)} "
+end
+
+def exponent (x,y)
+	puts "#{x} raised to the power of #{y} is #{x**y}"
+end
+
 begin
 	#Introduction to calculator
 	puts "Welcome to Calculator"
@@ -14,6 +43,11 @@ begin
 	puts "Advanced (a)"
 	calc_type = gets.chomp.to_s
 
+
+if calc_type == "s"
+
+# Simple Calculator
+
 	#Choosing operation to be performed (* / + -)
 	puts "To choose type of operation to be performed type:"
 	puts "Addition (a).\nSubtraction (s).\nMultiplication (m).\nDivision (d)."
@@ -23,29 +57,49 @@ begin
 	puts "Enter first number: "
 	num1 = gets.chomp.to_i
 
-	puts "Enter second number: "
-	num2 = gets.chomp.to_i
-
-	#Declaring methods
-	def addition (x,y)
-		puts "Your operation is: #{x} + #{y}"
-		x + y
+	while true
+		puts "Enter second number: "
+		num2 = gets.chomp.to_i
+		if (op_type == "d" && num2 == 0)
+			puts "Divisor cannot be 0"
+		else
+			break
+		end
 	end
 
-	def subtraction (x,y)
-		puts "Your operation is: #{x} - #{y}"
-		x - y
+
+elsif calc_type == "a"
+
+# Advanced Calculator
+
+	puts "To choose type of operation to be performed type: "
+	puts "Square Root (w).\nExponent (e)."
+	op_type = gets.chomp.to_s
+
+
+	if op_type == "w"
+		
+		puts "Enter number: "
+		num1 = gets.chomp.to_i
+
+	elsif op_type == "e"
+		
+		#Inputting two values
+		puts "Enter base number: "
+		num1 = gets.chomp.to_i
+
+		puts "Enter exponent number: "
+		num2 = gets.chomp.to_i
+
+	else
+		puts "Please select w or e"
 	end
 
-	def multiplication (x,y)
-		puts "Your operation is: #{x} * #{y}"
-		x * y
-	end
+else calc_type != "s" || "a"
+	puts "Invalid input. Please type s for the Simple Calculator or a for the Advanced Calculator"
+# we have to loop it to be true to out message
+end
 
-	def division (x,y)
-		puts "Your operation is: #{x} / #{y}"
-		puts x / y
-	end
 
 	#Operations
 	case op_type
@@ -57,6 +111,10 @@ begin
 			puts multiplication(num1,num2)
 		when "d"
 			puts division(num1,num2)
+		when "w"
+			puts square_root(num1)
+		when "e"
+			puts exponent(num1,num2)
 		else 
 			puts "Invalid input. Please try again."
 	end
